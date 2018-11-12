b014c7fb73a6744de2f0ce7a286d6e82abda7bae
adamjorycromer <adamjorycromer@gmail.com>
2018-11-11 22:30:38 -0700

    worked on char stats layout

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+let Data = {
+    health:320,
+    attack:16 
+};
+
+let Worf = {
+    health:270,
+    attack:15
+};
+
+let Troi = {
+    health:340,
+    attack:12 
+};
+
+let Geordi = {
+    health:280,
+    attack:14 
+};
+
+});
+
