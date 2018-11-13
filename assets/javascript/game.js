fb6922c39e57753aa7d47aebf3e473aedda67251
adamjorycromer <adamjorycromer@gmail.com>
2018-11-12 17:25:54 -0700

    fixed char stats

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,50 @@
 $(document).ready(function() {
 
 let Data = {
+    name:"Lieutenant Commander Data",
     health:320,
     attack:16 
 };
 
 let Worf = {
+    name:"",
     health:270,
     attack:15
 };
 
 let Troi = {
+    name:"",
     health:340,
     attack:12 
 };
 
 let Geordi = {
+    name:"",
     health:280,
     attack:14 
 };
 
-});
+let BorgDrone = {
+    name:"Borg Drone",
+    health:250,
+    attack:13
+};
 
+
+
+function updateCharInfo() {
+    document.getElementById("charHealth").innerHTML = Data.health;
+    document.getElementById("charAtt").innerHTML = Data.attack;
+    document.getElementById("charName").innerHTML = Data.name;
+
+    document.getElementById("enemyHealth").innerHTML = BorgDrone.health;
+    document.getElementById("enemyAtt").innerHTML = BorgDrone.attack;
+    document.getElementById("enemyName").innerHTML = BorgDrone.name;
+}
+
+
+
+updateCharInfo();
+
+
+});
