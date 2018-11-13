1c70c46e5eb592462eea40c56b672a0468a2a506
adamjorycromer <adamjorycromer@gmail.com>
2018-11-13 15:01:24 -0700

    show and hide functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -33,9 +33,11 @@ $(document).ready(function () {
     let charHealth = $("#charHealth");
     let charAtt = $("#charAtt");
     let charName = $("#charName");
+    let charChoice = $("#charChoice")
     let enemyHealth = $("#enemyHealth");
     let enemyAtt = $("#enemyAtt");
     let enemyName = $("#enemyName");
+    let enemyChoice = $("#enemyChoice")
     let hasPlayerChosen = false;
 
     function updateCharInfo() {
@@ -47,58 +49,124 @@ $(document).ready(function () {
         enemyName.html(BorgDrone.name);
     }
 
+    function showChar() {
+        $("#charStats").show();
+    }
+
+    function hideChar() {
+        $("#charStats").hide();
+    }
+
+    function showEnemy() {
+        $("#enemyStats").show();
+    }
+
+    function hideEnemy() {
+        $("#enemyStats").hide();
+    }
+
+    function hideEnemyList() {
+        $("#enemies").hide();
+    }
+
+    
+
+    function leaveHover() {
+        charChoice.attr('src', '/assets/images/placeholder.jpg');
+        charHealth.html("??");
+        charAtt.html("??");
+        charName.html("Name");
+        console.log("test");
+    }
+
     $("#data").hover(function () {
+        showChar();
+        charChoice.attr('src', '/assets/images/data.jpg');
         charHealth.html(Data.health);
         charAtt.html(Data.attack);
         charName.html(Data.name);
     });
     $("#worf").hover(function () {
+        showChar();
+        charChoice.attr('src', '/assets/images/worf.jpg');
         charHealth.html(Worf.health);
         charAtt.html(Worf.attack);
         charName.html(Worf.name);
     });
     $("#troi").hover(function () {
+        showChar();
+        charChoice.attr('src', '/assets/images/troi.jpg');
         charHealth.html(Troi.health);
         charAtt.html(Troi.attack);
         charName.html(Troi.name);
     });
     $("#geordi").hover(function () {
+        showChar();
+        charChoice.attr('src', '/assets/images/geordi.jpg');
         charHealth.html(Geordi.health);
         charAtt.html(Geordi.attack);
         charName.html(Geordi.name);
     });
 
-    if (hasPlayerChosen === false) {
+    //These hide the char stat box when you stop hovering.
+    $("#data").mouseleave(function () {
+        hideChar();
+    });
+
+    $("#worf").mouseleave(function () {
+        hideChar();
+    });
+
+    $("#troi").mouseleave(function () {
+        hideChar();
+    });
+
+    $("#geordi").mouseleave(function () {
+        hideChar();
+    });
+
+    // if (hasPlayerChosen === false) {
         $("#data").on("click", function () {
-            $("#data").hide();
-            
+            showChar();
+            $("#characters").hide();
+            charChoice.attr('src', '/assets/images/data.jpg');
             charHealth.html(Data.health);
             charAtt.html(Data.attack);
             charName.html(Data.name);
             hasPlayerChosen = true;
         });
         $("#worf").on("click", function () {
-            $("#worf").hide();
+            showChar();
+            $("#characters").hide();
+            charChoice.attr('src', '/assets/images/worf.jpg');
             charHealth.html(Worf.health);
             charAtt.html(Worf.attack);
             charName.html(Worf.name);
             hasPlayerChosen = true;
         });
         $("#troi").on("click", function () {
-            $("#troi").hide();
+            showChar();
+            $("#characters").hide();
+            charChoice.attr('src', '/assets/images/troi.jpg');
             charHealth.html(Troi.health);
             charAtt.html(Troi.attack);
             charName.html(Troi.name);
             hasPlayerChosen = true;
         });
         $("#geordi").on("click", function () {
-            $("#geordi").hide();
+            
+            $("#characters").hide();
+            charChoice.attr('src', '/assets/images/geordi.jpg');
             charHealth.html(Geordi.health);
             charAtt.html(Geordi.attack);
             charName.html(Geordi.name);
             hasPlayerChosen = true;
+            showChar();
         });
-    }
+    // }
 
+// hideChar();
+// hideEnemy();
+// hideEnemyList();
 
 });
